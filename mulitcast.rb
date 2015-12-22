791c0fff7f2d4c3e9e770db373632de17f381adf
Jayden Callahan <rails@rizon.net>
2015-12-22 19:14:22 +1030

    not even
    Signed-off-by: Jayden Callahan <rails@rizon.net>

---
+++ mulitcast.rb
@@ -0,0 +1,19 @@
+require 'ipaddr'
+require 'socket'
+require 'net/http'
+
+def mclisten
+  s = UDPSocket.open
+  MCA = "239.0.0.1"
+  MCP = 3300
+  m = IPAddr.new(MCA).hton + IPAddr.new("0.0.0.0").hton
+  s.setsockopt(:IPPROTO_IP, :IP_ADD_MEMBERSHIP, m)
+  s.setsockopt(:SOL_SOCKET, :SO_REUSEPORT, 1)
+  s.bind("0.0.0.0", MCP)
+  loop do
+    mess, _ = s.recvfrom(1024)
+    Net::HTTP.post_form(URI.parse('http://127.0.0.1:3000/c/put'), {'json'=>mess})
+  end
+end
+
+mclisten()
